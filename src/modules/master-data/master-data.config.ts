29799e39a86bde4602e819a989608ce361b59484
Wittawat_F <wittawat.f@softdebut.com>
2025-12-12 10:44:17 +0700

    #

--- src/modules/master-data/master-data.config.ts
+++ src/modules/master-data/master-data.config.ts
@@ -2,18 +2,16 @@ import { FpmaMasterDepartment } from 'src/entities/FpmaMasterDepartment';
 import { FpmaMasterDistrict } from 'src/entities/FpmaMasterDistrict';
 
 export const MASTER_CONFIG = {
-  // 'KEY' : ค่าที่จะให้ Frontend ส่งมา (เช่น /masters/district)
   district: {
-    entity: FpmaMasterDistrict, // ชื่อ Class Entity
-    value: 'code', // ชื่อ Column ที่เป็น ID (INT)
-    label: 'nameTh', // ชื่อ Column ที่จะโชว์เป็นชื่อ (Text)
-    orderBy: 'nameTh', // จะให้เรียงตามอะไร
+    entity: FpmaMasterDistrict,
+    value: 'code',
+    label: 'nameTh',
+    orderBy: 'nameTh',
   },
   department: {
     entity: FpmaMasterDepartment,
     value: 'itemId',
-    label: 'name', // เช็คใน Entity ดีๆ ว่าชื่อ field คือ name หรือ description
+    label: 'name',
     orderBy: 'itemOrder',
   },
-  // อยากเพิ่ม Table ไหนอีก ก็เพิ่มต่อตรงนี้ได้เลยครับ...
 };
