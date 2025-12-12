1734b114b2bc3e08a5c46ab33b5fc4ed92eb313b
Wittawat_F <wittawat.f@softdebut.com>
2025-12-12 10:44:44 +0700

    #

--- src/modules/master-data/master-data.controller.ts
+++ src/modules/master-data/master-data.controller.ts
@@ -1,9 +1,8 @@
-// src/modules/master-data/master-data.controller.ts
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { MasterDataService } from './master-data.service';
 
-@ApiTags('Master Data (Dropdowns)') // ชื่อที่จะโชว์ใน Swagger UI
+@ApiTags('Master Data (Dropdowns)')
 @Controller('masters')
 export class MasterDataController {
   constructor(private readonly masterDataService: MasterDataService) {}
